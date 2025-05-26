d4d433597f0ed60f1a17116767afdd9f432d55c5
kurutodenzeru <kurtdenzel51@gmail.com>
2025-05-26 13:12:16 +0800

    build: add nuxt-vitalizer module and update configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,19 +11,28 @@ export default defineNuxtConfig({
 		plugins: [
 			tailwindcss(),
 		],
+		build: {
+			sourcemap: false // Disable sourcemaps to avoid the warning
+		}
 	},
 
-	modules: ['shadcn-nuxt'],
+	modules: ['shadcn-nuxt', 'nuxt-vitalizer'],
 
 	shadcn: {
 		/**
-		 * Prefix for all the imported component
-		 */
+			* Prefix for all the imported component
+			*/
 		prefix: '',
 		/**
-		 * Directory that the component lives in.
-		 * @default "./components/ui"
-		 */
+			* Directory that the component lives in.
+			* @default "./components/ui"
+			*/
 		componentDir: './components/ui'
+	},
+
+	vitalizer: {
+		// Remove the render-blocking entry CSS
+		disableStylesheets: 'entry',
+		disablePrefetchLinks: true
 	}
 })
